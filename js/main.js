a5f6daa409c4fffd8310e5be766f54e4bda635e8
Andrey Baev <60654928+AbaevB@users.noreply.github.com>
2026-01-18 19:04:59 +0700

    delete old

--- js/main.js
+++
@@ -1,8 +0,0 @@
-import burger from "./components/burger.js";
-import changeLocation from "./components/changeLocation.js";
-import catalog from "./components/catalog.js";
-document.addEventListener('DOMContentLoaded', function(){
-    burger(); 
-    changeLocation();
-    catalog();
-});
